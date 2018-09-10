fd08f88d68302d233743330674ab7cf424ba1e3f
Zhibo Fan <zb1439@berkeley.edu>
2018-09-10 15:58:14 -0700

    OMG hw0

--- hw0/Tester.java
+++ hw0/Tester.java
@@ -45,5 +45,43 @@ public class Tester {
     public static void main(String[] unused) {
         textui.runClasses(Tester.class);
     }
+	private static int max_forloop(int[] a) {
+		int max = 0;
+		for(int i=0;i<a.length;i++)
+			if(a[i]>=max) max = a[i];
+		return max;
+	}
+	private static int max_whileloop(int[] a) {
+		int max = 0; int i = 0;
+		while(i<a.length) {
+			if(a[i]>=max) max = a[i];
+			i++;
+		}
+		return max;
+	}
+	//3sum solution
+	private static boolean threeSum(int[] a) {
+		if(a.length < 3) return false;//array size too small
+		for(int i=0;i<a.length;i++) {
+			for(int j=i;j<a.length;j++) {
+				for(int k=j;k<a.length;k++) {
+					if(a[i]+a[j]+a[k] == 0) return true;
+				}
+			}
+		}
+		return false;
+	}
+	//3sum-distinct solution
+	private static boolean threeSumDistinct(int[] a) {
+		if(a.length < 3) return false;//array size too small
+		for(int i=0;i<a.length;i++) {
+			for(int j=i+1;j<a.length-1;j++) {
+				for(int k=j+1;k<a.length-2;k++) {
+					if(a[i]+a[j]+a[k] == 0) return true;
+				}
+			}
+		}
+		return false;
+	}
 
 }
