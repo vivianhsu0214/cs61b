0371ed16c389d0f3b10a013757d65ec728a07e5f
Zhibo Fan <zb1439@berkeley.edu>
2018-10-04 16:19:37 -0700

    new class supporting extra test

---
+++ proj1/enigma/SymbolAlphabet.java
@@ -0,0 +1,37 @@
+package enigma;
+
+public class SymbolAlphabet extends Alphabet {
+    /**
+     * An alphabet containing given characters.
+     *
+     * @param chars string consists of all characters.
+     */
+    SymbolAlphabet(String chars) {
+        _content = chars;
+    }
+
+    @Override
+    int size() {
+        return _content.length();
+    }
+
+    @Override
+    boolean contains(char ch) {
+        return _content.contains(String.valueOf(ch));
+    }
+
+    @Override
+    char toChar(int index) {
+        return _content.charAt(index);
+    }
+
+    @Override
+    int toInt(char ch) {
+        return _content.indexOf(ch);
+    }
+
+    /**
+     * A string of all available characters.
+     */
+    private String _content;
+}
