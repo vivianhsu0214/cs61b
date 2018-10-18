e43004c7b16771f0f5a03b7c160bf5a33ba559d1
Zhibo Fan <zb1439@berkeley.edu>
2018-10-17 23:03:59 -0700

    Completed lab9

--- lab9/ArrayHeap.java
+++ lab9/ArrayHeap.java
@@ -120,65 +120,97 @@ public class ArrayHeap<T> {
     /* Returns the index of the node to the left of the node at i. */
     private int getLeftOf(int i) {
         //YOUR CODE HERE
-        return 0;
+        return i * 2;
     }
 
     /* Returns the index of the node to the right of the node at i. */
     private int getRightOf(int i) {
         //YOUR CODE HERE
-        return 0;
+        return i * 2 + 1;
     }
 
     /* Returns the index of the node that is the parent of the node at i. */
     private int getParentOf(int i) {
-        //YOUR CODE HERE
-        return 0;
+        return i / 2;
     }
 
     /* Adds the given node as a left child of the node at the given index. */
     private void setLeft(int index, Node n) {
         //YOUR CODE HERE
+        setNode(getLeftOf(index), n);
     }
 
     /* Adds the given node as the right child of the node at the given index. */
     private void setRight(int index, Node n) {
         //YOUR CODE HERE
+        setNode(getRightOf(index), n);
     }
 
     /** Returns the index of the node with smaller priority. Precondition: not
       * both nodes are null. */
     private int min(int index1, int index2) {
         //YOUR CODE HERE
-        return 0;
+        Node n1 = getNode(index1);
+        Node n2 = getNode(index2);
+        if (n1 == null || n2 == null) {
+            return -1;
+        }
+        return (n1.priority() > n2.priority() ? index2 : index1);
     }
 
     /* Returns the Node with the smallest priority value, but does not remove it
      * from the heap. */
     public Node peek() {
         //YOUR CODE HERE
-        return null;
+        return getNode(1);
     }
 
     /* Bubbles up the node currently at the given index. */
     private void bubbleUp(int index) {
         //YOUR CODE HERE
+        while(min(index, getParentOf(index)) == index) {
+            swap(index, getParentOf(index));
+            index = getParentOf(index);
+        }
     }
 
     /* Bubbles down the node currently at the given index. */
     private void bubbleDown(int index) {
         //YOUR CODE HERE
+        if (Integer.highestOneBit(index) == Integer.highestOneBit(size())
+                || 2 * index > size() || 2 * index + 1 > size()) {
+            return;
+        } else {
+            int minIdx = min(index, min(getLeftOf(index), getRightOf(index)));
+            if(index != minIdx) {
+                swap(index, minIdx);
+                bubbleDown(minIdx);
+            }
+        }
     }
 
     /* Inserts an item with the given priority value. Same as enqueue, or offer. */
     public void insert(T item, double priority) {
         //YOUR CODE HERE
+        Node n = new Node(item, priority);
+        setNode(size() + 1, n);
+        bubbleUp(size());
     }
 
     /* Returns the element with the smallest priority value, and removes it from
      * the heap. Same as dequeue, or poll. */
     public T removeMin() {
         //YOUR CODE HERE
-        return null;
+        if (size() == 0) {
+            return null;
+        } else if (size() == 1) {
+            return removeNode(1).item();
+        }
+        Node peek = getNode(1);
+        T result = peek.item();
+        setNode(1, removeNode(size()));
+        bubbleDown(1);
+        return result;
     }
 
     /* Changes the node in this heap with the given item to have the given
@@ -186,6 +218,23 @@ public class ArrayHeap<T> {
      * item. Check for item equality with .equals(), not == */
     public void changePriority(T item, double priority) {
         //YOUR CODE HERE
+        Node n = null;
+        int idx = 0;
+        for (int i = 1; i <= size(); i++) {
+            if (getNode(i).item().equals(item)) {
+                n = getNode(i);
+                idx = i;
+                break;
+            }
+        }
+        if (n == null) {
+            return;
+        }
+        n.setPriority(priority);
+        if (min(idx, getParentOf(idx)) == idx) {
+            bubbleUp(idx);
+        } else {
+            bubbleDown(idx);
+        }
     }
-
 }
--- lab9/ArrayHeapTest.java
+++ lab9/ArrayHeapTest.java
@@ -20,6 +20,47 @@ public class ArrayHeapTest {
         assertEquals(0, pq.size());
     }
 
+    static ArrayHeap<String> getTest() {
+        ArrayHeap<String> pq = new ArrayHeap<>();
+        pq.insert("A", 1);
+        pq.insert("B", 2);
+        pq.insert("C", 3);
+        pq.insert("D", 4);
+        pq.insert("E", 5);
+        pq.insert("F", 6);
+        pq.insert("G", 7);
+        pq.insert("H", 8);
+        pq.insert("I", 9);
+        pq.insert("J", 10);
+        pq.insert("K", 11);
+        pq.insert("L", 12);
+        pq.insert("M", 13);
+        pq.insert("N", 14);
+        pq.insert("O", 15);
+        return pq;
+    }
+
+    @Test
+    public void testRemoveAndInsert() {
+        ArrayHeap<String> pq = getTest();
+        pq.insert("A-B", 1.5);
+        pq.removeMin();
+        assertEquals("A-B", pq.removeMin());
+    }
+
+    @Test
+    public void testChangePriority() {
+        ArrayHeap<String> pq = getTest();
+        pq.changePriority("I", 0);
+        assertEquals( "I", pq.peek().item());
+        pq.changePriority("H", 2.5);
+        for (int i = 0; i < 3; i++) {
+            pq.removeMin();
+        }
+        assertEquals("H", pq.removeMin());
+        assertEquals("C", pq.removeMin());
+    }
+
     public static void main(String[] args) {
         System.exit(ucb.junit.textui.runClasses(ArrayHeapTest.class));
     }
