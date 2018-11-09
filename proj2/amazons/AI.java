a337f12d3d403dd2c56612dca35e52a5f272f228
Zhibo Fan <zb1439@berkeley.edu>
2018-11-08 22:26:05 -0800

    greedy search

--- proj2/amazons/AI.java
+++ proj2/amazons/AI.java
@@ -103,7 +103,7 @@ class AI extends Player {
             while (it.hasNext()) {
                 Move successor = it.next();
                 board.makeMove(successor);
-                int thisValue = findMove(board, depth,
+                int thisValue = findMove(board, depth - 1,
                         false, -sense, alpha, beta);
                 board.undo();
                 if (thisValue >= v) {
